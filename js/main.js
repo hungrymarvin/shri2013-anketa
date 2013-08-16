8faf3ef25254f96a582117af2425584f5278e785
Maxim Myalin <maximrazor@gmail.com>
2013-08-16 23:54:06 +0400

    add basic html, css, js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
+$('.task').click(function () {
+    $(this).toggleClass('collapsed');
+})
 
+$('textarea').click(function (e) {
+    e.stopPropagation();
+})
+$('input').click(function (e) {
+    e.stopPropagation();
+})
+    .focus(function () {
+        $(this).closest('.task').addClass('pending');
+    })
+
+$('input').focusout(function () {
+    $(this).closest('.task').removeClass('pending');
+
+    if ($(this).val().length != 0) {
+        $(this).closest('.task').addClass('success').removeClass('error');
+        //глубокая проверка
+    }
+    else {
+        $(this).closest('.task').removeClass('success').removeClass('error');
+    }
+})
+
+$('#anketa').submit(function () {
+    checkForm();
+    return false;
+})
+
+function checkForm() {
+    var foundErrorFlag = false;
+
+    $('input').each(function () {
+
+        if (!$(this).val()) {
+            if (!foundErrorFlag) {
+                $('.btn > input').val('Есть ошибки в заполнении')
+                $(this).focus().closest('.task').addClass('error').removeClass('collapsed')
+                $('#anketa').addClass('error');
+                foundErrorFlag = true;
+            }
+        }
+        else {
+            $(this).closest('.task').addClass('success').removeClass('error');
+        }
+    })
+}
